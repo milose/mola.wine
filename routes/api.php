d510c46d33a6e11fabd49cc63073ef6557c6111c
Milos Sakovic <m@deva.co>
2017-07-26 17:06:06 +0200

    Rename the api endpoint for Venues.

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::get('locations', function() {
+Route::get('venues', function() {
     return App\Venue::all();
 });
