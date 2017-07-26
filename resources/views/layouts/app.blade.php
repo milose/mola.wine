eba7b045a300d08eab8f0eab2cb10114d25171b2
Milos <milose@users.noreply.github.com>
2017-07-26 21:33:00 +0200

    Remove parent div around content yield directive.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <div class="container">
-        @yield('content')
-    </div>
+    @yield('content')
 
     <!-- Scripts -->
     @stack('scripts')
