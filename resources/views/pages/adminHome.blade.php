8052948f4ca2f9b3e3d0173862d34ef20a36dec4
Milos Sakovic <m@deva.co>
2017-07-26 18:26:54 +0200

    Add the adminHome view.

---
+++ resources/views/pages/adminHome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="title">Welcome {{ auth()->user()->name }}</div>
+        <hr>
+        <div><a href="{{ action('VenueController@index') }}" class="button is-primary"><i class="fa fa-fv fa-map-marker"></i> &nbsp; Venues</a></div>
+        <hr>
+        <div><a href="{{ action('Auth\RegisterController@showRegistrationForm') }}" class="button is-warning"><i class="fa fa-fv fa-plus"></i> &nbsp; Add new user</a></div>
+        <hr>
+        <div>
+            <form action="{{ action('Auth\LoginController@logout') }}" method="post">
+                {{ csrf_field() }}
+
+                <button type="submit" class="button is-danger"><i class="fa fa-fv fa-sign-out"></i> &nbsp; Logout</button>
+            </form>
+        </div>
+    </section>
+@endsection
