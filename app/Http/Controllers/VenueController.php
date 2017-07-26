c15df64444afb2e966d8c4abc65ceb4d4e0f69d5
Milos Sakovic <m@deva.co>
2017-07-26 18:06:14 +0200

    Add the adminHome route and auth group to the routes.

--- app/Http/Controllers/VenueController.php
+++ app/Http/Controllers/VenueController.php
@@ -6,11 +6,6 @@ use App\Venue;
 
 class VenueController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         //
