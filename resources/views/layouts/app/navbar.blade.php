ce88bb568b22eac809a3a7fc2fa8fefef126c799
Milos Sakovic <m@deva.co>
2017-07-29 13:42:50 +0200

    Fix the navbar issues.

--- resources/views/layouts/app/navbar.blade.php
+++ resources/views/layouts/app/navbar.blade.php
@@ -13,54 +13,58 @@
 
   <div id="navMenu" class="navbar-menu">
     <div class="navbar-start">
+
       <a class="navbar-item " href="/">
         Home
       </a>
-      <a class="navbar-item " href="/">
+
+      <a class="navbar-item " href="/products">
         Products
       </a>
-      <a class="navbar-item " href="/">
+
+      <a class="navbar-item " href="/contact">
         Contact
       </a>
+
+        @if (!Auth::guest())
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link is-active" href="/home">
+              Admin
+            </a>
+            <div class="navbar-dropdown">
+              <div class="navbar-item">
+                <div>
+                  <p class="has-text-info is-size-6-desktop"><strong>{{ Auth::user()->name }}</strong></p>
+
+                    <small>
+                      <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                              document.getElementById('logout-form').submit();">
+                          Logout
+                      </a>
+                    </small>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+
+                </div>
+              </div>
+              <hr class="navbar-divider">
+              <a class="navbar-item" href="/venues">
+                Venues
+              </a>
+              <a class="navbar-item" href="{{ route('register') }}">
+                Add New User
+              </a>
+            </div>
+          </div>
+        @endif
+
     </div>
 
     <div class="navbar-end">
-      @if (!Auth::guest())
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link  is-active" href="/home">
-            Admin
-          </a>
-          <div class="navbar-dropdown">
-            <div class="navbar-item">
-              <div>
-                <p class="has-text-info is-size-6-desktop"><strong>{{ Auth::user()->name }}</strong></p>
-
-                  <small>
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                            document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
-                  </small>
-
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                      {{ csrf_field() }}
-                  </form>
-
-              </div>
-            </div>
-            <hr class="navbar-divider">
-            <a class="navbar-item" href="/venues">
-              Venues
-            </a>
-            <a class="navbar-item" href="{{ route('register') }}">
-              Add New User
-            </a>
-          </div>
-        </div>
-      @endif
-
       <div class="navbar-item">
-        <div class="field is-grouped">
+        <div class="field">
           <p class="control">
             <a class="button" href="https://fb.me/molawine" style="color: #3B5998;">
               <span class="icon">
