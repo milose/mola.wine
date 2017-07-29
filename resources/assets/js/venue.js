69c4d0511c331a88cda4a92f4c3aca32c5afb77a
Milos Sakovic <m@deva.co>
2017-07-29 19:42:34 +0200

    Remove show current location from admin panel.

--- resources/assets/js/venue.js
+++ resources/assets/js/venue.js
@@ -3,7 +3,6 @@ const app = window['app'] = new Vue({
 
   data: {
     currentLocation: window.mapCenterLocation,
-    readingLocation: false,
     address: '',
   },
 
@@ -23,30 +22,6 @@ const app = window['app'] = new Vue({
         this.currentLocation.lat = pos.lat()
         this.currentLocation.lng = pos.lng()
       })
-    },
-
-    showCurrentLocation() {
-      this.readingLocation = true
-
-      // read current location
-      navigator.geolocation.getCurrentPosition(acquired => {
-          this.currentLocation = {
-            lat: acquired.coords.latitude,
-            lng: acquired.coords.longitude,
-            zoom: 18,
-          }
-
-          // center map
-          try {
-            this.map.setCenter(new google.maps.LatLng(this.currentLocation.lat, this.currentLocation.lng))
-            this.map.setZoom(this.currentLocation.zoom)
-          } catch (err) {}
-
-          this.readingLocation = false
-        },
-        () => {
-          this.readingLocation = false
-        })
     },
 
     blurInput(event) {
